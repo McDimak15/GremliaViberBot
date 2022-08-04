d5ee150fb5a53192c9e048ecd24f12b3eae614f8
McDimak15 <59174406+McDimak15@users.noreply.github.com>
2022-08-04 12:49:32 +0300

    Add files via upload

---
+++ Viberbot/set_webhook.php
@@ -0,0 +1,21 @@
+<?php
+	$auth_token = '4f9a0f4dcba7e2e7-f37075034c9079ab-2b245fb5f1932f38';
+	$webhook = 'WEBHOOK_URL';
+	
+	$jsonData = 
+	'{
+		"auth_token": "'.$auth_token.'",
+		"url": "'.$webhook.'",
+		"event_types": ["subscribed", "unsubscribed", "delivered", "message", "seen"]
+	}';
+	
+	$ch = curl_init('https://chatapi.viber.com/pa/set_webhook');
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	$response = curl_exec($ch);
+	$err = curl_error($ch);
+	curl_close($ch);
+	if($err) {echo($err);}
+	else {echo($response);}
+?>
